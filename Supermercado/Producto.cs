b8e9e241252d0e77c6ba0c851ed7c5612c8546cc
NataliCastellanosG <nmcastellanos63145@umanizales.edu.co>
2025-03-25 21:19:47 -0500

    Creación de la clase producto

---
+++ Supermercado/Producto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermercado
+{
+    public class Producto
+    {
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string marca { get; set; }
+
+        private float valor;
+
+        private float descuento;
+
+        public float Valor
+        {
+            get { return valor; }
+            set
+            {
+                if (value >= 0)
+                    valor = value;
+                else
+                    Console.WriteLine('\n' + "El valor no puede ser negativo");
+            }
+        }
+
+        public float Descuento
+        {
+            get { return descuento; }
+            set
+            {
+                if (value >= 0 && value <= 60) descuento = value;
+                else Console.WriteLine('\n' + "El descuento debe estar entre 0% y 60%");
+            }
+        }
+
+        public Producto(string _nombre, string _descripcion, string _marca, float _valor, float _descuento)
+        {
+            nombre = _nombre;
+            descripcion = _descripcion;
+            marca = _marca;
+            Valor = _valor;
+            Descuento = _descuento;
+        }
+
+        public void Describir()
+        {
+            Console.WriteLine("Producto: " + nombre);
+            Console.WriteLine("Descripción: " + descripcion);
+            Console.WriteLine("Marca: " + marca);
+            Console.WriteLine("Valor: " + Valor);
+            Console.WriteLine("Descuento: " + Descuento);
+        }
+
+    }
+}
+
--- Supermercado/Program.cs
+++ Supermercado/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Supermercado;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Producto producto = new Producto(
+            "Arroz",
+            "Arroz premium",
+            "Diana",
+            5000,
+            55);
+
+        producto.Describir();
+
+        Producto producto2 = new Producto(
+            "Café",
+            "Nescafé 750 gr ",
+            "Nescafé",
+            10000,
+            65);
+
+        Console.WriteLine("\n");
+
+        producto2.Describir();
+    }
+}
